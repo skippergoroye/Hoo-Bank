8f4c598089ba1d904002c71d44e7aabaf52c030f
skippergoroye@gmail.com <oluwatosinadegoroye2@gmail.com>
2023-04-23 14:14:07 -0500

    stats added

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { arrowUp } from "../assets";
 
 const GetStarted = () => {
   return (
-    <div className='flex justify-center items-center w-[140px] h-[140px] rounded-full bg-blue-gradient'>
-      GetStarted
-    </div>
-  )
-}
+    <div className="flex justify-center items-center w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer">
+      <div className="flex justify-center items-center flex-col bg-primary w-[100%] h-[100%] rounded-full">
+        <div className="flex justify-center items-start flex-row">
+          <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
+            <span className="text-gradient">Get</span>
+          </p>
+            <img src={arrowUp} alt="arrow" className="w-[23px] h-[23px] object-contain" />
+        </div>
 
-export default GetStarted
+        <p className="font-poppins font-medium text-[18px] leading-[23px]">
+            <span className="text-gradient">Started</span>
+        </p>
+
+      </div>
+    </div>
+  );
+};
+
+export default GetStarted;
